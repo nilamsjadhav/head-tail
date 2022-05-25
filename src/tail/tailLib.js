2738c3b50a7c0dc39971f10d2ffd4147edec1ee7
Nilam Jadhav <99525537+nilamsjadhav@users.noreply.github.com>
2022-05-25 09:59:11 +0530

    Made tail work for default behaviour.

--- src/tail/tailLib.js
+++ src/tail/tailLib.js
@@ -1,3 +1,7 @@
-const lastNLines = content => content;
+const lastNLines = (content, numOfLines) => {
+  const lines = content.split('\n');
+  const index = lines.length - numOfLines;
+  return lines.slice(index).join('\n');
+};
 
 exports.lastNLines = lastNLines;
--- test/tail/testTailLib.js
+++ test/tail/testTailLib.js
@@ -15,4 +15,10 @@ describe('lastNLines', () => {
   it('should give all line if lines less than 10', () => {
     assert.strictEqual(lastNLines('1\n2\n3\n4\n5'), '1\n2\n3\n4\n5');
   });
+  it('should give last 10 lines.', () => {
+    const content = '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12';
+    const expected = '3\n4\n5\n6\n7\n8\n9\n10\n11\n12';
+    assert.strictEqual(lastNLines(content, 10), expected);
+  });
+
 });
